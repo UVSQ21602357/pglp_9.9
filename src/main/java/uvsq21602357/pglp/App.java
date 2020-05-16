be3625386e1a7bfa5aba3cf17370b22fb29eba45
Clément <clement.carre@ens.uvsq.fr>
2020-05-16 14:51:09 +0200

    Init du maven

---
+++ src/main/java/uvsq21602357/pglp/App.java
@@ -0,0 +1,13 @@
+package uvsq21602357.pglp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
