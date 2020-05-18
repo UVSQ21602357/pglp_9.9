ea4057fb9a7a187fde238a93adaedb7e3532af09
Clément <clement.carre@ens.uvsq.fr>
2020-05-18 17:30:10 +0200

    Lecture creation DrawingTUI'

--- src/main/java/uvsq21602357/DrawingTUI.java
+++ src/main/java/uvsq21602357/DrawingTUI.java
@@ -10,21 +10,70 @@ public class DrawingTUI {
 	}
 	
 	private void nextCommand() {
+		
+		//Création
 		System.out.print("Commande = ");
 		String str = sc.nextLine();
 		while(str.equals("Fin") == false) {
 			
-			String[] s = str.split("=");
-			String[] s2 = s[1].split("\\(\\(");
-			String[] s3 = s2[1].split("\\);");
-			String[] s4 = s3[0].split(",");
-			//String[] s5 = s3[1].split("\\)");
-			System.out.println("s = " + s[0]);
-			System.out.println("s2 = " + s2[0]);
-			System.out.println("s4[0] = " + s4[0]);
-			System.out.println("s4[1] = " + s4[1]);
-			System.out.println("s3[1] = " + s3[1]);
-			//System.out.println("s5 = " + s5[0]);
+			if(str.equals("Create") || str.equals("create")) {
+				str = sc.nextLine();
+				String[] s = str.split("=");
+				String[] s2 = s[1].split("\\(\\(");
+				if(s2[0].equals("Cercle") || s2[0].equals("cercle")) {
+					String[] s3 = s2[1].split("\\),");
+					String[] s4 = s3[0].split(",");
+					String[] s5 = s3[1].split("\\)");
+					/*System.out.println("s = " + s[0]);
+					System.out.println("s2 = " + s2[0]);
+					System.out.println("s4[0] = " + s4[0]);
+					System.out.println("s4[1] = " + s4[1]);
+					System.out.println("s5 = " + s5[0]);*/
+				}
+				else if(s2[0].equals("Rectangle") || s2[0].equals("rectangle")) {
+					String[] s3 = s2[1].split("\\),\\(");
+					String[] s4 = s3[0].split(",");
+					String[] s5 = s3[1].split(",");
+					String[] s6 = s5[1].split("\\)");
+					System.out.println("s2 = " + s2[0]);
+					System.out.println("s4[0] = " + s4[0]);
+					System.out.println("s4[1] = " + s4[1]);
+					System.out.println("s5 = " + s5[0]);
+					System.out.println("s6 = " + s6[0]);
+					//c1=Rectangle((0,0),(10,15))
+				}
+				else if(s2[0].equals("Carré") || s2[0].equals("carré")) {
+					String[] s3 = s2[1].split("\\),\\(");
+					String[] s4 = s3[0].split(",");
+					String[] s5 = s3[1].split(",");
+					String[] s6 = s5[1].split("\\)");
+					System.out.println("s2 = " + s2[0]);
+					System.out.println("s4[0] = " + s4[0]);
+					System.out.println("s4[1] = " + s4[1]);
+					System.out.println("s5 = " + s5[0]);
+					System.out.println("s6 = " + s6[0]);
+				}
+				else if(s2[0].equals("Triangle") || s2[0].equals("triangle")) {
+					//T1=triangle((0,0),(10,10),(15,25))
+					String[] s3 = s2[1].split("\\),\\(");
+					String[] s4 = s3[0].split(",");
+					String[] s5 = s3[1].split(",");
+					String[] s6 = s5[1].split("\\),\\(");
+					String[] s7 = s3[2].split(",");
+					String[] s8 = s7[1].split("\\)");
+					System.out.println("s2 = " + s2[0]);
+					System.out.println("s4[0] = " + s4[0]);
+					System.out.println("s4[1] = " + s4[1]);
+					System.out.println("s5 = " + s5[0]);
+					System.out.println("s6 = " + s6[0]);
+					System.out.println("s7 = " + s7[0]);
+					System.out.println("s8 = " + s8[0]);
+				}
+			}
+
+
+			
+			
 			System.out.print("Commande = ");
 			str = sc.nextLine();
 		}
